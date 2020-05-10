c59e9d001856eb194697c76d8bf4252fa7252dc6
Michal <m.bozyk00@gmail.com>
2020-05-10 21:26:15 +0200

    requirements, workspace

--- biblioteka/settings.py
+++ biblioteka/settings.py
@@ -119,15 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
-STATICFILES_DIRS = [
+STATICFILES_STORAGE = [
 
 "/Users/michalbozyk/biblioteka/books/static/css/style.css"
 ]
 
 django_heroku.settings(locals())
 
-STATIC_ROOT = []
-STATICFILES_STORAGE = []
 
